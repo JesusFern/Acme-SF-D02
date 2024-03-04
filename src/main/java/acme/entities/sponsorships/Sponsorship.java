c292b59dc2b31d36c12420eaf9590f09b32b1f28
Miguel González Ortiz <miggonort1@alum.us.es>
2024-03-04 23:19:14 +0100

    Relation Project created

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -17,6 +19,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -58,4 +61,9 @@ public class Sponsorship extends AbstractEntity {
 
 	@URL
 	private String				link;
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	private Project				project;
 }
