c669ec4c98ac90fe2c507f6b45f613ec6e0b0cd0
luiperfra1 <luiperfra1@alum.us.es>
2024-03-04 18:41:36 +0100

    Task-I2--004--Client

---
+++ src/main/java/acme/forms/Client.java
@@ -0,0 +1,22 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+
+public class Client extends AbstractForm {
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	Double						totalNumberOfProgressLogsBelow25;
+	Double						totalNumberOfProgressLogsBetween25to50;
+	Double						totalNumberOfProgressLogsBetween50to75;
+	Double						totalNumberOfProgressLogsAbove25;
+
+	Double						ContractsBudget;
+	Double						deviationProjectsCost;
+	Double						minimumContractsBudget;
+	Double						maximumContractsBudget;
+
+}
