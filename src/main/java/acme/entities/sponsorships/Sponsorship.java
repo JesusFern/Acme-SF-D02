4d1369bc3ebb88543cfaac00d29143c3f7c66a6c
Miguel González Ortiz <miggonort1@alum.us.es>
2024-03-04 23:28:04 +0100

    Relation Sponsorship created

--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -58,4 +60,9 @@ public class Sponsorship extends AbstractEntity {
 
 	@URL
 	private String				link;
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	private Sponsorship			sponsorship;
 }
