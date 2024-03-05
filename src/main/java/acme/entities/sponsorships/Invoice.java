319eeab62d7dca2a0aebdaf4642028611fcb07d1
Miguel González Ortiz <miggonort1@alum.us.es>
2024-03-05 14:29:43 +0100

    dueDate created

--- src/main/java/acme/entities/sponsorships/Invoice.java
+++ src/main/java/acme/entities/sponsorships/Invoice.java
@@ -10,6 +10,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.validation.Valid;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -44,11 +45,18 @@ public class Invoice extends AbstractEntity {
 	@NotNull
 	private Date				registrationTime;
 
-	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Future
 	@NotNull
-	private Date				dueDate;
+	private Date				startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				endDate;
 
 	@NotNull
+	@Valid
 	@Min(1)
 	private Integer				quantity;
 
@@ -60,7 +68,7 @@ public class Invoice extends AbstractEntity {
 	@Transient
 	public Double totalAmount() {
 		Double result;
-		result = this.getQuantity() + this.getTax();
+		result = this.getQuantity() * this.getTax();
 		return result;
 	}
 
