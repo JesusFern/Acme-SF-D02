b0e4acac69eff60a6485d7df80888fde0dc3041f
javigrci <javgarrod5@alum.us.es>
2024-03-07 00:07:19 +0100

    Fix: Task-I3---001--Training Module

--- src/main/java/acme/entities/trainingModule/TrainingModule.java
+++ src/main/java/acme/entities/trainingModule/TrainingModule.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -16,6 +18,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -59,4 +62,11 @@ public class TrainingModule extends AbstractEntity {
 	private String				link;
 
 	private int					time;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
 }
