40f1d5e7bf3d230124716b3fe372cd49c4c61ca3
Miguel González Ortiz <miggonort1@alum.us.es>
2024-03-06 21:17:24 +0100

    Fix constraints

--- src/main/java/acme/entities/sponsorships/Invoice.java
+++ src/main/java/acme/entities/sponsorships/Invoice.java
@@ -12,7 +12,7 @@ import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
@@ -35,12 +35,12 @@ public class Invoice extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}", message = "{validation.pattern} + IN-0244-4532")
 	@Column(unique = true)
 	private String				code;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
+	@PastOrPresent
 	@NotNull
 	private Date				registrationTime;
 
@@ -58,23 +58,27 @@ public class Invoice extends AbstractEntity {
 	@Positive
 	private double				tax;
 
+	@URL
+	private String				link;
+
+	// Derived attributes ----------------------------------------------------------
+
 
 	@Transient
 	public Money totalAmount() {
 		Money res = new Money();
 		Double result;
-		result = this.getQuantity().getAmount() * this.getTax();
+		result = this.getQuantity().getAmount() + this.getQuantity().getAmount() * (this.getTax() / 100);
 		res.setAmount(result);
 		res.setCurrency(this.getQuantity().getCurrency());
 		return res;
 	}
 
+	// Relationships ----------------------------------------------------------
 
-	@URL
-	private String		link;
 
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
-	private Sponsorship	sponsorship;
+	private Sponsorship sponsorship;
 }
