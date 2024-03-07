f3721d0d60434520354020de8e94e848abedd7f2
luiperfra1 <luiperfra1@alum.us.es>
2024-03-07 13:41:25 +0100

    Progress-log.csv

--- src/main/java/acme/entities/progressLogs/ProgressLog.java
+++ src/main/java/acme/entities/progressLogs/ProgressLog.java
@@ -9,11 +9,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import acme.client.data.AbstractEntity;
@@ -34,7 +36,9 @@ public class ProgressLog extends AbstractEntity {
 	@Pattern(regexp = "^PG-[A-Z]{1,2}-[0-9]{4}$", message = "{validation.pattern} + PG-A-1111")
 	private String				recordId;
 
-	@Positive
+	@Digits(integer = 3, fraction = 2)
+	@Max(100)
+	@Min(0)
 	private double				percentageCompleteness;
 
 	@NotBlank
