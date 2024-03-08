cda23979a61121f96e8c1804933776762238e3f6
javigrci <javgarrod5@alum.us.es>
2024-03-08 20:39:04 +0100

    Reports Student 3, Fix UML Student 3 & Fix Task-I3---001-Training Module

--- src/main/java/acme/entities/trainingModule/TrainingModule.java
+++ src/main/java/acme/entities/trainingModule/TrainingModule.java
@@ -9,6 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
@@ -61,6 +62,7 @@ public class TrainingModule extends AbstractEntity {
 	@URL
 	private String				link;
 
+	@Min(0)
 	private int					time;
 
 	// Relationships ----------------------------------------------------------
